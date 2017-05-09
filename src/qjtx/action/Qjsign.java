7f15f2818785c62ae9609bb236f11545fb4b5f63
joeyDluffy <joey8522@163.com>
2017-05-10 07:32:11 +0800

    sign problem

--- src/qjtx/action/Qjsign.java
+++ src/qjtx/action/Qjsign.java
@@ -1,70 +1,32 @@
 package qjtx.action;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import com.opensymphony.xwork2.Action;
 import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
 
-import qjtx.pojo.Operationuser;
-import qjtx.pojo.Qjoperaters;
-import qjtx.pojo.SalesDataStore;
-import qjtx.service.QjmanageService;
-import qjtx.service.SalesService;
-import sun.misc.BASE64Decoder;
 
 @Controller
 public class Qjsign extends ActionSupport {
 
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7304053604230479092L;
+	private static final String appSecret = "62b83473b750a7fe07ab8bdb5fb42ba5";
+	
 	@Override
 	public String execute() throws Exception {
-		
 
+//		String sign = Hex(HmacMD5(appSecret+"appkey"+appkey+"instance_id"+instance_id +"ts"+ts+appSecret));
 		return SUCCESS;
 
 	}
 
 	private boolean success;
-	private String username;
-	private String userdisplayname;
-	private int reviewopid;
-
-	public int getReviewopid() {
-		return reviewopid;
-	}
-
-	public void setReviewopid(int reviewopid) {
-		this.reviewopid = reviewopid;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public String getUserdisplayname() {
-		return userdisplayname;
-	}
-
-	public void setUserdisplayname(String userdisplayname) {
-		this.userdisplayname = userdisplayname;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	private String password;
-
+	
 	public boolean isSuccess() {
 		return success;
 	}
@@ -72,5 +34,75 @@ public class Qjsign extends ActionSupport {
 	public void setSuccess(boolean success) {
 		this.success = success;
 	}
+	
+	public String getTs() {
+		return ts;
+	}
 
+	public void setTs(String ts) {
+		this.ts = ts;
+	}
+
+	public String getAppkey() {
+		return appkey;
+	}
+
+	public void setAppkey(String appkey) {
+		this.appkey = appkey;
+	}
+
+	public String getInstance_id() {
+		return instance_id;
+	}
+
+	public void setInstance_id(String instance_id) {
+		this.instance_id = instance_id;
+	}
+
+	public String getSign() {
+		return sign;
+	}
+
+	public void setSign(String sign) {
+		this.sign = sign;
+	}
+
+	public String getItemId() {
+		return itemId;
+	}
+
+	public void setItemId(String itemId) {
+		this.itemId = itemId;
+	}
+
+	public String getTradeToken() {
+		return tradeToken;
+	}
+
+	public void setTradeToken(String tradeToken) {
+		this.tradeToken = tradeToken;
+	}
+
+	public String getMixUserId() {
+		return mixUserId;
+	}
+
+	public void setMixUserId(String mixUserId) {
+		this.mixUserId = mixUserId;
+	}
+
+	private String ts;
+	private String appkey;
+	private String instance_id;
+	private String sign;
+	
+	private String itemId;
+	private String tradeToken;
+	private String mixUserId;
+
+	
+	
+	
+	
+	
 }
--- src/util/MD5Util.java
+++ src/util/MD5Util.java
@@ -1,52 +1,104 @@
 package util;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 import java.security.MessageDigest;
+import javax.crypto.KeyGenerator;
+import javax.crypto.Mac;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 
-/** 
- * 采用MD5加密解密 
- */ 
+import com.taobao.api.Constants;
+
+/**
+ * 采用MD5加密解密
+ */
 public class MD5Util {
-	/*** 
-     * MD5加码 生成32位md5码 
-     */  
-    public static String string2MD5(String inStr){  
-        MessageDigest md5 = null;  
-        try{  
-            md5 = MessageDigest.getInstance("MD5");  
-        }catch (Exception e){  
-            System.out.println(e.toString());  
-            e.printStackTrace();  
-            return "";  
-        }  
-        char[] charArray = inStr.toCharArray();  
-        byte[] byteArray = new byte[charArray.length];  
-  
-        for (int i = 0; i < charArray.length; i++)  
-            byteArray[i] = (byte) charArray[i];  
-        byte[] md5Bytes = md5.digest(byteArray);  
-        StringBuffer hexValue = new StringBuffer();  
-        for (int i = 0; i < md5Bytes.length; i++){  
-            int val = ((int) md5Bytes[i]) & 0xff;  
-            if (val < 16)  
-                hexValue.append("0");  
-            hexValue.append(Integer.toHexString(val));  
-        }  
-        return hexValue.toString();  
-  
-    }  
-  
-    /** 
-     * 加密解密算法 执行一次加密，两次解密 
-     */   
-    public static String convertMD5(String inStr){  
-  
-        char[] a = inStr.toCharArray();  
-        for (int i = 0; i < a.length; i++){  
-            a[i] = (char) (a[i] ^ 't');  
-        }  
-        String s = new String(a);  
-        return s;  
-  
-    }  
+	/**
+	 * MAC算法可选以下多种算法
+	 *
+	 * <pre>
+	 * HmacMD5
+	 * HmacSHA1
+	 * HmacSHA256
+	 * HmacSHA384
+	 * HmacSHA512
+	 * </pre>
+	 */
+	public static final String KEY_MAC = "HmacMD5";
+
+	public static byte[] encryptHMAC(String data, String secret) throws IOException {
+	    byte[] bytes = null;
+	    try {
+	        SecretKey secretKey = new SecretKeySpec(secret.getBytes(Constants.CHARSET_UTF8), "HmacMD5");
+	        Mac mac = Mac.getInstance(secretKey.getAlgorithm());
+	        mac.init(secretKey);
+	        bytes = mac.doFinal(data.getBytes(Constants.CHARSET_UTF8));
+	    } catch (GeneralSecurityException gse) {
+	        throw new IOException(gse.toString());
+	    }
+	    return bytes;
+	}
+	 
+	public static String byte2hex(byte[] bytes) {
+	    StringBuilder sign = new StringBuilder();
+	    for (int i = 0; i < bytes.length; i++) {
+	        String hex = Integer.toHexString(bytes[i] & 0xFF);
+	        if (hex.length() == 1) {
+	            sign.append("0");
+	        }
+	        sign.append(hex.toUpperCase());
+	    }
+	    return sign.toString();
+	}
+    public static void main(String[] args)throws Exception{
+    	String sign="E2B068F2E85AF42882C747F169ACBC8B";
+        String inputStr = "62b83473b750a7fe07ab8bdb5fb42ba5appkey23759189instance_id254ts149434892762662b83473b750a7fe07ab8bdb5fb42ba5";
+        byte[] inputData = inputStr.getBytes();
+        String key = "UTF_8";
+        System.out.println(MD5Util.byte2hex(MD5Util.encryptHMAC(inputStr,"62b83473b750a7fe07ab8bdb5fb42ba5")));
+    }
+	/***
+	 * MD5加码 生成32位md5码
+	 */
+	public static String string2MD5(String inStr) {
+		MessageDigest md5 = null;
+		try {
+			md5 = MessageDigest.getInstance("MD5");
+		} catch (Exception e) {
+			System.out.println(e.toString());
+			e.printStackTrace();
+			return "";
+		}
+		char[] charArray = inStr.toCharArray();
+		byte[] byteArray = new byte[charArray.length];
+
+		for (int i = 0; i < charArray.length; i++)
+			byteArray[i] = (byte) charArray[i];
+		byte[] md5Bytes = md5.digest(byteArray);
+		StringBuffer hexValue = new StringBuffer();
+		for (int i = 0; i < md5Bytes.length; i++) {
+			int val = ((int) md5Bytes[i]) & 0xff;
+			if (val < 16)
+				hexValue.append("0");
+			hexValue.append(Integer.toHexString(val));
+		}
+		return hexValue.toString();
+
+	}
+
+	/**
+	 * 加密解密算法 执行一次加密，两次解密
+	 */
+	public static String convertMD5(String inStr) {
+
+		char[] a = inStr.toCharArray();
+		for (int i = 0; i < a.length; i++) {
+			a[i] = (char) (a[i] ^ 't');
+		}
+		String s = new String(a);
+		return s;
+
+	}
 
 }
